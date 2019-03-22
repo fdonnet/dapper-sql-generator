3629abfc23ea6351042275ef89e6c916b8cc0ced
Fabien Donnet <fabien.donnet@ubiko.ch>
2019-03-22 16:07:03 +0100

    Pk: field name for the method name selectByPk => ex: selectById if the pk is "id"

--- SqlGenerator/StoredProcedures/SqlSelectByPKGenerator.cs
+++ SqlGenerator/StoredProcedures/SqlSelectByPKGenerator.cs
@@ -38,6 +38,14 @@ namespace SqlGenerator.StoredProcedures
                 })
             );
 
+            var pkFieldName = String.Join("And",
+                pkColumns.Select(col =>
+                {
+                    var colName = col.Name.Parts[2];
+                    return $"{TSqlModelHelper.PascalCase(colName)}";
+                })
+            );
+
             var grants = String.Join(Environment.NewLine + Environment.NewLine,
                 GrantExecuteTo.Select(roleName =>
                     "GRANT EXECUTE" + Environment.NewLine
@@ -52,7 +60,7 @@ $@"
 -- Description:	Select By PK Procedure for the table {Table.Name} 
 -- =================================================================
 
-CREATE PROCEDURE [dbo].[usp{TSqlModelHelper.PascalCase(Table.Name.Parts[1])}_selectByPK]
+CREATE PROCEDURE [dbo].[usp{TSqlModelHelper.PascalCase(Table.Name.Parts[1])}_selectBy{pkFieldName}]
 (
 {inputParamDeclarations}
 )
