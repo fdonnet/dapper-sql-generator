8486434652f9362992f2393b17dc09a2417615be
Marco Di Tullio <marcoditullio@hotmail.it>
2019-03-22 09:40:52 +0100

    initial commit

---
+++ SqlGenerator.UI/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.SqlServer.Dac.Model;
+using SqlGenerator.DotNetClient;
+using SqlGenerator.StoredProcedures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SqlGenerator.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        TSqlModel Model { get; set; } = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void ButtonLoadModel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                buttonLoadModel.IsEnabled = false;
+                Model = await Task.Run(() => TSqlModelHelper.LoadModel("PensionLab.DB.dacpac"));
+                MessageBox.Show("Model loaded successfully", "Info", MessageBoxButton.OK, MessageBoxImage.Information);               
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                buttonLoadModel.IsEnabled = true;
+            }
+        }
+
+
+        private void ButtonShowTables_Click(object sender, RoutedEventArgs e)
+        {
+            var tables = Model.GetAllTables();
+            lstTables.ItemsSource = tables;
+            lstTables.DisplayMemberPath = "Name.Parts[1]";
+        }
+
+        private void ButtonGenerateDelete_Click(object sender, RoutedEventArgs e)
+        {
+            TSqlObject table = lstTables.SelectedItem as TSqlObject;
+            if (table != null)
+            {
+                DeleteGenerator gen = new DeleteGenerator(table, grantExecuteTo: new string[] { "role_admin", "role_user" });
+                var output = gen.Generate();
+                txtOutput.Text = output;
+            }
+        }
+
+        private void ButtonGenerateInsert_Click(object sender, RoutedEventArgs e)
+        {
+            TSqlObject table = lstTables.SelectedItem as TSqlObject;
+            if (table != null)
+            {
+                InsertGenerator gen = new InsertGenerator(table, grantExecuteTo: new string[] { "role_admin", "role_user" });
+                var output = gen.Generate();
+                txtOutput.Text = output;
+            }
+        }
+
+        private void ButtonGenerateNetEntityClass_Click(object sender, RoutedEventArgs e)
+        {
+            TSqlObject table = lstTables.SelectedItem as TSqlObject;
+            if (table != null)
+            {
+                EntityClassGenerator gen = new EntityClassGenerator(table, classNamespace: "PensionLab.DTO");
+                var output = gen.Generate();
+                txtOutput.Text = output;
+            }
+        }
+    }
+}
---
+++ SqlGenerator/DotNetClient/EntityClassGenerator.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.SqlServer.Dac.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlGenerator.DotNetClient
+{
+    public class EntityClassGenerator
+    {
+        public TSqlObject Table { get; private set; } = null;
+
+        public string Author { get; private set; } = "Author";
+
+        public string ClassNamespace { get; private set; } = "Entities";
+
+
+        public EntityClassGenerator(TSqlObject table, string author = null, string classNamespace = null)
+        {
+            this.Table = table;
+            this.Author = author ?? this.Author;
+            this.ClassNamespace = classNamespace ?? this.ClassNamespace;
+        }
+
+
+        public string Generate()
+        {
+            var allColumns = Table.GetAllColumns().Where(col => !col.GetProperty<bool>(Column.IsIdentity));
+
+            var memberDeclarations = String.Join(Environment.NewLine + "        ", allColumns.Select(col =>
+            {
+                var memberName = TSqlModelHelper.PascalCase(col.Name.Parts[2]);
+                var colDataType = col.GetColumnSqlDataType(false);
+                var isNullable = col.IsColumnNullable();
+                var memberType = TSqlModelHelper.GetDotNetDataType(colDataType, isNullable);
+
+                var decorators = "";
+                if (memberType == "string")
+                {
+                    var colLen = col.GetProperty<int>(Column.Length);
+                    if (colLen > 0)
+                    {
+                        decorators += $"[StringLength({colLen})]"
+                            + Environment.NewLine + "        ";
+                    }
+                }
+                if (!isNullable)
+                {
+                    decorators += $"[Required]"
+                            + Environment.NewLine + "        ";
+                }
+
+                return $"{decorators}public {memberType} {memberName} {{ get; set; }}";
+            }));
+
+            string output =
+$@" 
+-- =================================================================
+-- Author: {this.Author}
+-- Description:	Entity class for the table {Table.Name} 
+-- =================================================================
+
+namespace { ClassNamespace } {{
+  
+    public class { TSqlModelHelper.PascalCase(Table.Name.Parts[1]) } : ICloneable
+    {{ 
+        
+        { memberDeclarations }
+
+
+        public object Clone()
+        {{
+            return this.MemberwiseClone();
+        }}
+
+    }}
+}}
+
+";
+
+            return output;
+        }
+
+
+    }
+}
+
+
