1bea599412f2812a260361328980040ba4362f71
fdonnet <fabien.donnet@ubiko.ch>
2019-03-28 16:11:09 +0100

    Inetrface repo first version need to be cleaned a lot and we will see when we begin with rela class repo

--- SqlGenerator/DotNetClient/CsRepositoryClassGenerator.cs
+++ SqlGenerator/DotNetClient/CsRepositoryClassGenerator.cs
@@ -33,10 +33,15 @@ namespace SqlGenerator.DotNetClient
 
         private string InterfaceDeclaration()
         {
+            //Var needed to generate
             var interfaceName = "I" + TSqlModelHelper.PascalCase(Table.Name.Parts[1]) + "Repo";
             var entityName = TSqlModelHelper.PascalCase(Table.Name.Parts[1]);
+            var pkColumns = Table.GetPrimaryKeyColumns();
+            var uks = Table.GetUniqueKeysWithColumns();
 
-            var methodDeclarations = String.Join(Environment.NewLine + "        ", GetMethodsSignaturesForInterface(entityName));
+
+            var methodDeclarations = String.Join(Environment.NewLine + "        ",
+                GetMethodsSignatures(entityName,pkColumns, uks, true));
 
             string output =
 $@" 
@@ -60,34 +65,107 @@ namespace { _settings.Namespace} {{
             return output;
         }
 
-        private IEnumerable<string> GetMethodsSignaturesForInterface(string entityName)
+        private IEnumerable<string> GetMethodsSignatures(string entityName, IEnumerable<TSqlObject> pkColumns,
+            IEnumerable<IEnumerable<TSqlObject>> uks, bool forInterface = false)
+
         {
-            if (_globalSettings.GenerateBulkInsertSP)
-                yield return ""; //To be defined
+            string funcBegin = forInterface ? string.Empty : "public async ";
 
+            //Get all
             if (_globalSettings.GenerateSelectAllSP)
-                yield return $"Task<IEnumerable<{entityName}>> GetAll();";
+                yield return $"{funcBegin}Task<IEnumerable<{entityName}>> GetAll();";
 
+            //Get by PK
             if (_globalSettings.GenerateSelectByPk)
-                yield return $"Task<{entityName}> GetById(int id);"; //only works with ID as a pk, hard coded, need to be changed to support composit PK
-
-            if(_globalSettings.GenerateSelectByUK)
             {
-                //TODO
+                var pkFieldNames = String.Join("And",
+                      pkColumns.Select(col =>
+                      {
+                        var colName = col.Name.Parts[2];
+                        return $"{TSqlModelHelper.PascalCase(colName)}";
+                      })
+                    );  
+
+                var pkFieldsWithType = String.Join(", ",
+                      pkColumns.Select(col =>
+                      {
+                          var colName = col.Name.Parts[2];
+                          var colDataType = col.GetColumnSqlDataType(false);
+                          //Search for custom member type or use the conversion from Sql Types
+                          var memberType = (_globalSettings.CsEntitySettings.FieldNameCustomTypes != null 
+                                    && _globalSettings.CsEntitySettings.FieldNameCustomTypes.ContainsKey(colName)
+                                                           ? _globalSettings.CsEntitySettings?.FieldNameCustomTypes[colName]
+                                                          : TSqlModelHelper.GetDotNetDataType(colDataType, false));
+
+                          return $"{memberType} {FirstCharacterToLower(TSqlModelHelper.PascalCase(colName))}";
+                      })
+                    );
+
+                yield return $"{funcBegin}Task<{entityName}> GetBy{pkFieldNames}({pkFieldsWithType});"; 
             }
 
-            if(_globalSettings.GenerateInsertSP)
-                yield return $"Task<int> Insert({entityName} my{entityName});";
+            //Get by uks
+            if (_globalSettings.GenerateSelectByUK)
+            {
+                foreach(var ukColumns in uks)
+                {
+                    var ukFieldNames = String.Join("And",
+                    ukColumns.Select(col =>
+                    {
+                        var colName = col.Name.Parts[2];
+                        return $"{TSqlModelHelper.PascalCase(colName)}";
+                    }));
 
+                    var ukFieldsWithType = String.Join(", ",
+                        ukColumns.Select(col =>
+                     {
+                         var colName = col.Name.Parts[2];
+                         var colDataType = col.GetColumnSqlDataType(false);
+                          //Search for custom member type or use the conversion from Sql Types
+                          var memberType = (_globalSettings.CsEntitySettings.FieldNameCustomTypes != null
+                                   && _globalSettings.CsEntitySettings.FieldNameCustomTypes.ContainsKey(colName)
+                                                          ? _globalSettings.CsEntitySettings?.FieldNameCustomTypes[colName]
+                                                         : TSqlModelHelper.GetDotNetDataType(colDataType, false));
+
+                         return $"{memberType} {FirstCharacterToLower(TSqlModelHelper.PascalCase(colName))}";
+                     })
+                   );
+
+                    yield return $"{funcBegin}Task<{entityName}> GetBy{ukFieldNames}({ukFieldsWithType});";
+                }
+            }
+
+            //Insert
+            if (_globalSettings.GenerateInsertSP)
+                yield return $"{funcBegin}Task<int> Insert({entityName} {FirstCharacterToLower(entityName)});";
+
+            //Bulk insert
+            //if (_globalSettings.GenerateBulkInsertSP)
+            //    yield return ""; //To be defined
+
+            //Update
             if (_globalSettings.GenerateUpdateSP)
-                yield return $"Task<bool> Update({entityName} my{entityName});";
+                yield return $"{funcBegin}Task<bool> Update({entityName} {FirstCharacterToLower(entityName)});";
 
+            //Delete
             if (_globalSettings.GenerateDeleteSP)
-                yield return $"Task<bool> Delete(int id);"; //only work with and int id as pk, hard coded need to be changed
+                yield return $"{funcBegin}Task<bool> Delete(int id);"; //only work with and int id as pk, hard coded need to be changed
 
 
         }
 
+        /// <summary>
+        /// Helper to convert first char to lower
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string FirstCharacterToLower(string str)
+        {
+            if (String.IsNullOrEmpty(str) || Char.IsLower(str, 0))
+                return str;
+
+            return Char.ToLowerInvariant(str[0]) + str.Substring(1);
+        }
 
     }
 }
